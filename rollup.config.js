03b957acccf55cbc5e9ff5c44279d4541b988005
ivanesik <elyoskin@gmail.com>
2023-02-25 15:40:43 +0100

    Feature: chulk in result bundle

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,5 @@
 import typescript from '@rollup/plugin-typescript';
+import {nodeResolve} from '@rollup/plugin-node-resolve';
 
 export default {
     input: 'src/index.mts',
@@ -6,10 +7,17 @@ export default {
         file: 'bundle.mjs',
         format: 'es',
     },
-    external: ['chalk', 'ws', 'fs', 'path'],
+    external: [
+        'fs',
+        'path',
+        // ws can't be bundled with Rollup and ESM (RollupError: "default" is not exported)
+        // Need to research
+        'ws',
+    ],
     plugins: [
         typescript({
             sourceMap: false,
         }),
+        nodeResolve({modulesOnly: true}),
     ],
 };
