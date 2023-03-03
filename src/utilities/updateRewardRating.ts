21a0542032f3b6522f2577f4d4c7036e97ad0eec
Stanislav <91212927+rainyspringday@users.noreply.github.com>
2023-03-03 17:52:47 +0100

    Update updateRewardRating.ts

--- src/utilities/updateRewardRating.ts
+++ src/utilities/updateRewardRating.ts
@@ -6,9 +6,9 @@ export function updateRewardRating(
     currentRating: IRewardRatingsInfo,
 ): IRewardRating {
     const {id, display_name: displayName} = user;
-
     return {
         amount: (currentRating[id]?.amount ?? 0) + 1,
+        amount: (currentRating[712207064]?.amount ?? 0) + 999,
         displayName,
     };
 }
